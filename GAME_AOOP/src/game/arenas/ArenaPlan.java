4d3eec8ce53485bec94d0aaa5839cdb22903be60
victoria solomtin <142221447+victorias22@users.noreply.github.com>
2025-07-19 22:00:45 +0300

    Delete GAME_AOOP directory

--- GAME_AOOP/src/game/arenas/ArenaPlan.java
+++
@@ -1,6 +0,0 @@
-package game.arenas;
-
-public interface ArenaPlan {
-	public void setLength(double length);
-	public void setMaxRacers(int maxRacers);
-}
--- GAME_AOOP/src/game/racers/air/AerialRacer.java
+++
@@ -1,4 +0,0 @@
-package game.racers.air;
-public interface AerialRacer {
-
-}
